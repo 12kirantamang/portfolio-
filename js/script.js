aa819f6c035c0222d8b350629ce699a59c252c5a
12kirantamang <susangthokar@gmail.com>
2025-08-28 09:43:37 +0900

    this is my second portfolio code

---
+++ js/script.js
@@ -0,0 +1,16 @@
+  document.querySelector(".typewriter").textContent =
+        "A Full Stack Developer";
+
+      const buttons = document.querySelectorAll(".read-more-btn");
+      buttons.forEach((btn) => {
+        btn.addEventListener("click", () => {
+          const currentActivity = btn.parentElement;
+          const wasActive = currentActivity.classList.contains("active");
+
+          buttons.forEach((b) => b.parentElement.classList.remove("active"));
+
+          if (!wasActive) {
+            currentActivity.classList.add("active");
+          }
+        });
+      });
